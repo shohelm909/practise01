b6b507b99941e20247adf0f25747566b35cb71ff
shohelm909 <64063886+shohelm909@users.noreply.github.com>
2023-12-04 17:57:00 +0600

    ternary operators

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-let age = 30;
+/*let age = 30;
 if(age<=18){
     document.write("not Adult");
 }else{
     document.write("adult");
-}
+}*/
+
+
+let age = 20;
+var type = (age <= 10) ? "not adult" : (age <=20) ? "not enough adult" : "adult";
+document.write(type);
