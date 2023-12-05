79ed18c0abe6efa99e17ebf8026f58f0be49e4a4
shohelm909 <64063886+shohelm909@users.noreply.github.com>
2023-12-05 18:23:37 +0600

    Array find method

--- script.js
+++ script.js
@@ -1,59 +1,29 @@
-/*let age = 30;
-if(age<=18){
-    document.write("not Adult");
-}else{
-    document.write("adult");
-}*/
+let numbers = [1,2,3,4,5,10,9];
+var text = numbers.find(function(value,index,array){
+    return value > 5;
+})
+document.write(text);
 
+class Student{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
 
-let country = "Bangladesh is my homeland"
-
-var text1 = country.length;
-document.write(text1,"<br/>");
-var text2 = country.slice(10,13);
-document.write(text2,"<br/>");
-var text3 = country.slice(16,25);
-document.write(text3,"<br/>");
-var text4 = country.slice(10)
-document.write(text4,"<br/>");
-var text5 = country.slice(-25,-8)
-document.write(text5,"<br/>");
-var text6 = country.slice(-8);
-document.write(text6,"<br/>")
-
-function myFunction(){
-    let text = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = text.replace(/microsoft/i,"Windows");
+    test(){
+        document.write("hello");
+    }
+    exampleFunction(){
+        let arr = [1,3,4,5];
+       /* arr.find(function(){
+            this.test();
+        },this);*/
+        arr.find(()=>{this.test()});
+    }
 }
 
-var text7 = "I love cats. Cats are very easy to love. Cats are very popular";
-text7 = text7.replaceAll("Cats", "dogs");
-document.write(text7,"<br/>");
-var text8 = "I love cats. Cats are very easy to love. Cats are very popular";
-var text8 = text8.toUpperCase();
-document.write(text8,"<br/>")
-var text9 = "I love cats. Cats are very easy to love. Cats are very popular";
-text9 = text9.toLowerCase();
-document.write(text9,"<br/>");
-var text8 = "I love cats. Cats are very easy to love. Cats are very popular";
-var text9 = "and cow";
 
-text8 = text8.concat(" ", text9);
-document.write(text8,"<br/>");
 
-var text10 = " Md SHohel ";
-text10 = text10.trim();
-text10 = text10.trimStart();
-text10 = text10.trimEnd();
-document.write(text10,"<br/>");
-/*var text11 = " Md SHohel ";
-text11 = text11.charAt(5);
-document.write(text11);*/
-var text12 = " Md SHohel ";
-const myArr = text12.split("");
-var text = "";
-for(var i=0; i <myArr.length;i++){
-    text += myArr[i] + "<br/>"
-}
+let student = new Student("Shohel", 35);
 
-document.getElementById("demo2").innerHTML = text;
+student.exampleFunction();
