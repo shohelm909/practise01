649655e0f3ca7762116aa4b1a070b1ce0ebcb0aa
shohelm909 <64063886+shohelm909@users.noreply.github.com>
2023-12-06 18:33:59 +0600

    Array method 01

--- script.js
+++ script.js
@@ -1,57 +1,65 @@
-var country = "Bangladesh is my country";
+var arr =[1,2,3,4,5,10,8];
+document.write(arr,"<br/>")
+var newArr = arr.length;
+document.write(newArr, "<br/>");
+newArr = arr.toString()
+document.write(newArr,"<br/>");
+newArr = arr.pop();
+document.write(newArr,"<br/>");
+document.write(arr.pop(),"<br/>");
+document.getElementById("demo").innerHTML = arr.pop();
 
-country = country.length;
-document.write(country)
-var country2 = "Bangladesh is my country";
-var country1 = country2.slice(10,24);
-country1 = country2.slice(0,10);
-country1 = country2.slice(-24,-7);
-document.write(country1)
+var fruits = ["banna","orange","Apple","Jackfruit"];
+document.write(fruits.pop(),"<br/>")
+document.write(fruits,"<br/>")
+document.write(fruits.push("dragonfruits"),"<br/>");
+document.write(fruits,"<br/>");
+ var fruits1 = fruits.shift();
+ document.write(fruits1,"<br/>");
+ document.write(fruits,"<br/>")
+ fruits1= fruits.unshift("Goava");
+ document.write(fruits,"<br/>")
+ fruits[2] = "not fruit";
+ document.write(fruits,"<br/>");
+ fruits[fruits.length] = "another fruits";
+ document.write(fruits,"<br/>")
 
-function myFunction(){
-    let text = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = text.replace(/microsoft/i,"windows");
-}
+ //array elements can be deleted using the javascript operator delete.
+ //using delete leaves undefined holes in the array
 
-var cats = "i love dogs.not cat. cat is not my favourite";
-var newCats = cats.replaceAll("cat","dogs");
-document.write(newCats);
+ var fruits = ["Banana", "Orange", "Apple", "Mango"];
 
-var cats = "i love dogs.not cat. cat is not my favourite";
-cats = cats.toUpperCase();
-document.write(cats);
-var cats = "i love dogs.not cat. cat is not my favourite";
-cats = cats.toLowerCase();
-document.write(cats);
+ document.getElementById("demo2").innerHTML = "the first fruit in : " + fruits[0];
+ fruits1 = delete fruits[0];
+ document.getElementById("demo3").innerHTML = "the seco fruit is : " + fruits[0];
 
-var cats = "Hello world  ";
-var cats1 = cats.trim();
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+var fruits1 = ["jackfruit", "coconut"];
+document.write(fruits.concat(fruits1),"<br/>")
+var newFruits = fruits1.concat(fruits);
+document.write(newFruits.toString(),"<br/>") 
 
-document.write(`lenght of cats: ${cats.length}, <br/> length of cats1 : ${cats1.length}<br/>`);
-cats1 = cats.trimEnd();
-document.write(`lenght of cats: ${cats.length}, <br/> lenght of cats1:${cats1.length}<br/>`);
-cats1 = cats.trimStart();
-document.write(`length of cats: ${cats.length},<br/> lenght of cats1: ${cats1.length},<br/>`)
-var cats= "my name is Shohel";
-var cats3 = "Hello world  ";
-cats1 = cats.charAt(5);
-document.write(cats1,"<br/>")
-var newCats = cats3.concat(cats);
-document.write(" ", newCats, "<br/>");
-var cats= "bangladesh";
+//Flattering an array is the process of reducing the dimensionality of an array
+//the flat() method creates a new array with sub-array elements concatenated to a specified depth
 
-var newCats = cats.split(",");
-var text = " ";
-for (var i=0;i<newCats.length;i++){
-    text += newCats[i] + "<br/>"
-}
+var myArr =[[1,2],[3,4],[5,6],[7,8]];
+var newArr = myArr.flat();
+document.write(newArr,"<br/>")
 
-document.write(text);
-var cats= "bangladesh";
 
-var text = " ";
-for (var i=0; i<cats.length;i++){
-    text += cats[i] + "<br/>"
-}
-document.write(text);
+//(Javascript Array Splice())  the first parameter difines the position where new element should be added(spliced in)
+//the second parameter defines how many elements should be removed
 
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(0,0, "lemon", "Kiwi");
+document.write(fruits,"<br/>");
+
+//the splice() method returns an array with the deleted items
+let remove = fruits.splice(1,2);
+document.write(remove,"<br/>")
+
+
+//(JavaScript Array Slice()) the slice() method slices out a piece of an array into a new array
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits = fruits.slice(0,2);
+document.write(fruits,"<br/>")
